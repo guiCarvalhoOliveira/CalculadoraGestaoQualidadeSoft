999948728918726b1c30d745b703d1d3e4441267
Guilherme <gui.carvalho0308@gmail.com>
2026-03-17 19:22:04 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.security.spec.RSAOtherPrimeInfo;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Calculadora calculadora = new Calculadora();
+
+        System.out.println(calculadora.somar(10,20));
+        System.out.println(calculadora.subtrair(300,100));
+        System.out.println(calculadora.multiplicar(10,2));
+        System.out.println(calculadora.dividir(10,5));
+
+    }
+}
